2a2b81258ea01958848e69d24070317b9eb2cf9a
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-01-11 15:43:55 +0530

    Add files via upload

---
+++ Practice exercise/searching/SmallestLetter.java
@@ -0,0 +1,28 @@
+package search;
+
+public class SmallestLetter {
+    public static void main(String[] args) {
+        char[] letters = {'a', 'c', 'g', 'm'};
+        char target = 'x';
+        System.out.println(nextGreatestLetter(letters, target));
+
+}
+
+    static char nextGreatestLetter(char[] letters, char target) {
+
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+           int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
+
