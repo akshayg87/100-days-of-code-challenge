2788a549fbaf2810efc35dcb84d18fcadc76d21e
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-01-14 15:15:49 +0530

    Guess the number

---
+++ Practice exercise/searching/guessnum.java
@@ -0,0 +1,26 @@
+package search;
+
+public class guessnum {
+
+//    static int guessno(int n)
+//    {
+//        int start = 0, end = n;
+//
+//        for (int i = 0; i < n ; i++) {
+//            int mid = start + (end - start)/2;
+//            int res = guess(mid);
+//            // guess() is a in-built function within Leetcode
+//            if(res == -1)
+//            {
+//                mid = mid - 1;
+//            }
+//            else if (res == 1)
+//            {
+//                mid = mid + 1;
+//            }
+//            else
+//                return mid;
+//        }
+//        return -1;
+//    }
+}
