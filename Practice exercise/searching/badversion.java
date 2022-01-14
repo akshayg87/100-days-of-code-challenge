823a3072b2d6da3345bd6cba6ee7c4f2ecb1bbd6
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-01-14 15:15:28 +0530

    Bad Version

---
+++ Practice exercise/searching/badversion.java
@@ -0,0 +1,24 @@
+package search;
+
+//public class badversion {
+//    static int num (int n[])
+//    {
+//        int start = 0, end = n.length-1, res = 0;
+//        while(start<=end)
+//        {
+//            int mid = start + (end - start)/2;
+//            if (isBoolean(mid))
+//            //isBoolean() is a in-built function within Leetcode
+//            {
+//                res = mid;
+//                end = mid -1;
+//            }
+//            else
+//            {
+//                start = mid + 1;
+//            }
+//        }
+//        return res;
+//    }
+//}
+//
