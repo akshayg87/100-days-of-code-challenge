0b6bbd1548259aac85758f5278345fd8bf878f9e
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-01-22 15:48:25 +0530

    Contains Duplicate

---
+++ Practice exercise/sorting/containsDuplicate.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class containsDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 2, 1};
+        bubbleSort(arr);
+        System.out.println(duplicate(arr));
+    }
+
+    static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    static boolean duplicate(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
