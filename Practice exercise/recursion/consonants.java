06c886042ec8db4cba3c6b1682f157233596d6f9
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-02-04 15:57:17 +0530

    Number of consonants

---
+++ Practice exercise/recursion/consonants.java
@@ -0,0 +1,32 @@
+public class consonants {
+    public static void main(String[] args)
+    {
+        String str = "abc de";
+        System.out.println(totalConsonants(str, str.length()));
+    }
+    static boolean isConsonant(char ch)
+    {
+        ch = Character.toUpperCase(ch);
+
+        return !(ch == 'A' || ch == 'E' ||
+                ch == 'I' || ch == 'O' ||
+                ch == 'U') && ch >= 65 && ch <= 90;
+    }
+
+
+    static int totalConsonants(String str, int n)
+    {
+        if (n == 1)
+        {
+            if(isConsonant(str.charAt(0)))
+                return 1;
+            else
+                return 0;
+        }
+
+        if(isConsonant(str.charAt(n - 1)))
+            return totalConsonants(str, n - 1) + 1;
+        else
+            return totalConsonants(str, n - 1);
+    }
+}
