f479e622924ffb0da617c350bab436330d77691f
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-02-05 16:09:22 +0530

    Reshape the matrix to given size

---
+++ Practice exercise/arrays/reshapeMatrix.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class reshapeMatrix {
+    
+        static int[][] matrixReshape(int[][] mat, int r, int c) {
+            int row = mat.length;
+            int col = mat[0].length;
+            int[][] ans = new int[r][c];
+            if(r*c != row*col) {
+                return mat;
+            } else {
+                int colp = 0;
+                int rowp = 0;
+                for(int i = 0; i < row; i++) {
+                    for(int j = 0; j < col; j++) {
+                        ans[rowp][colp] = mat[i][j];
+                        colp++;
+                        if(colp == c) {
+                            colp = 0;
+                            rowp++;
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+}
