dd4d3d76b04e7609044cb4c02db5ecd87072a83a
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-02-22 21:15:18 +0530

    Factorial

--- Practice exercise/recursion/factorial.java
+++ Practice exercise/recursion/factorial.java
@@ -1,17 +1,13 @@
-package recursion;
-
 public class factorial {
     public static void main(String[] args) {
-        int n = 3;
+       int n = 5;
         System.out.println(fact(n));
-
     }
 
     static int fact(int n)
     {
-        if (n <= 1)
-            return n;
-
-        return n * fact(n-1);
+        if (n<=1)
+            return 1;
+        return n*fact(n-1);
     }
 }
