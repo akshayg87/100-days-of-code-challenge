5b71f5bc1585a00d677c6a2bb64d65f4af5b8f1d
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-02-23 20:15:06 +0530

    Array sorted or not

--- Practice exercise/recursion/sorted.java
+++ Practice exercise/recursion/sorted.java
@@ -1,23 +1,14 @@
-package recursion;
-
 public class sorted {
     public static void main(String[] args) {
-        int[] arr = {12,2,3};
-        int n = arr.length;
-        if (sort(arr, n) != 0)
-            System.out.println("No");
-        else
-            System.out.println("Yes");
+        int[] arr = {1,2,3,4,5,7};
+        System.out.println(sort(arr,0));
     }
 
-    static int sort(int[] arr, int n)
+    static boolean sort(int[] arr, int index)
     {
-        if (n==1 || n==0)
-            return 0;
+        if (index == arr.length - 1)
+            return true;
 
-        if (arr[n-1] < arr[n-2])
-            return 1;
-
-        return sort(arr, n-1);
+        return arr[index] < arr[index+1] && sort(arr, index+1);
     }
 }
