0ff24fb87b9256531db84ac22f215f1b3e19062f
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-02-26 14:59:29 +0530

    Skip a character

---
+++ Practice exercise/recursion/Stream.java
@@ -0,0 +1,19 @@
+public class Stream {
+    public static void main(String[] args){
+        System.out.println(skip("bccabc"));
+    }
+
+    static String skip(String up){
+        if (up.isEmpty()){
+           return "";
+        }
+
+        char ch = up.charAt(0);
+        if (ch == 'a'){
+            return skip(up.substring(1));
+        }
+        else{
+            return ch + skip(up.substring(1));
+        }
+    }
+}
