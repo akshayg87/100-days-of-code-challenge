1ae9259a76c682337551644dd9247326a664b089
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-02-26 15:00:05 +0530

    Skip a string if it's not the required string

---
+++ Practice exercise/recursion/skipNot.java
@@ -0,0 +1,18 @@
+public class skipNot {
+    public static void main(String[] args){
+        System.out.println(skip("bccapplebc"));
+    }
+
+    static String skip(String up){
+        if (up.isEmpty()){
+            return "";
+        }
+
+        if (up.startsWith("app") && !up.startsWith("apple")){
+            return skip(up.substring(3));
+        }
+        else{
+            return up.charAt(0) + skip(up.substring(1));
+        }
+    }
+}
