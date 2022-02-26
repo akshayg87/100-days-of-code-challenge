9220bd55dd77f776fff6375e0259c5762ef02562
Akshay G <95754754+akshayg87@users.noreply.github.com>
2022-02-26 14:59:46 +0530

    Skip a string

---
+++ Practice exercise/recursion/skipString.java
@@ -0,0 +1,18 @@
+public class skipString {
+    public static void main(String[] args){
+    System.out.println(skip("bccapplebc"));
+}
+
+    static String skip(String up){
+        if (up.isEmpty()){
+            return "";
+        }
+
+        if (up.startsWith("apple")){
+            return skip(up.substring(5));
+        }
+        else{
+            return up.charAt(0) + skip(up.substring(1));
+        }
+    }
+}
